4122d45ebed41f2e7fd7686d69f5e61dc154908e
Oleg Syniakevych <Oleg@Olegs-MacBook-Pro.local>
2018-12-01 21:42:58 +0200

    vanilla js printing productss

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Http = new XMLHttpRequest();
-const url='http://nit.tron.net.ua/api/category/list';
+const url='http://nit.tron.net.ua/api/product/list';
 
 window.onload=function(){
 	Http.open("GET", url);
@@ -7,19 +7,23 @@ window.onload=function(){
 	Http.onreadystatechange=(e)=>{
 		if (Http.readyState != 4) return;
 		var response = JSON.parse(Http.responseText);
-		
+		console.log(response)
 		for (var i = 0; i < response.length; ++i) {
-			var div = document.createElement('div');
+			var currentTovar = response[i];
 
 			var nameDiv = document.createElement('div');
-			var descriptionDiv = document.createElement('div');
-
-			var currentTovar = response[i];
 			nameDiv.innerText = currentTovar.name;
+			
+			var descriptionDiv = document.createElement('div');
 			descriptionDiv.innerText = currentTovar.description;
 
+			var img = document.createElement('img');
+			img.setAttribute('src', currentTovar.image_url)
+
+			var div = document.createElement('div');
 			div.appendChild(nameDiv);
 			div.appendChild(descriptionDiv);
+			div.appendChild(img);
 
 			document.body.appendChild(div);
 		}
