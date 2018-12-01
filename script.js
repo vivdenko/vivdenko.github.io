996b1c2c88a9404d11674dfd4e096c59c8f09a87
Oleg Syniakevych <Oleg@Olegs-MacBook-Pro.local>
2018-12-01 21:56:16 +0200

    printing products with jquery

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-const Http = new XMLHttpRequest();
 const url='http://nit.tron.net.ua/api/product/list';
 
 window.onload=function(){
-	Http.open("GET", url);
-	Http.send();
-	Http.onreadystatechange=(e)=>{
-		if (Http.readyState != 4) return;
-		var response = JSON.parse(Http.responseText);
-		console.log(response)
-		for (var i = 0; i < response.length; ++i) {
-			var currentTovar = response[i];
+	$.get('http://nit.tron.net.ua/api/product/list', function( data ) {
+		for (var i = 0; i < data.length; ++i) {
+			var currentTovar = data[i];
 
-			var nameDiv = document.createElement('div');
-			nameDiv.innerText = currentTovar.name;
+			var nameDiv = $('<div></div>');
+			nameDiv.text(currentTovar.name);
 			
-			var descriptionDiv = document.createElement('div');
-			descriptionDiv.innerText = currentTovar.description;
+			var descriptionDiv = $('<div></div>');
+			descriptionDiv.text(currentTovar.description);
 
-			var img = document.createElement('img');
-			img.setAttribute('src', currentTovar.image_url)
+			var img = $('<img class="someclass" />');
+			img.attr('src', currentTovar.image_url);
 
-			var div = document.createElement('div');
-			div.appendChild(nameDiv);
-			div.appendChild(descriptionDiv);
-			div.appendChild(img);
+			var div = $('<div></div>');
+			div.append(nameDiv);
+			div.append(descriptionDiv);
+			div.append(img);
 
-			document.body.appendChild(div);
-		}
-	}
+			$('body').append(div);
+		}	
+	});
 }
